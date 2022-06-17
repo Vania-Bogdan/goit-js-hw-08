b8650109c0125a8d02ffbef25b282e1c3206f4fc
Vania Bogdan <bogdan.vania@gmail.com>
2022-06-17 13:01:27 +0300

    Create 03-feedback.js

---
+++ src/js/03-feedback.js
@@ -0,0 +1,39 @@
+import throttle from "lodash.throttle";
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('input');
+const message = document.querySelector('textarea');
+const STORAGE_KEY = 'feedback-form-state';
+
+form.addEventListener('input', throttle(onTextRead, 500));
+form.addEventListener('submit', onFormSubmit);
+
+function onTextRead(event) {
+  const dataStorage = {
+    email: email.value,
+    message: message.value,
+  }
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(dataStorage));
+};
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  console.log(localStorage.getItem(STORAGE_KEY));
+  localStorage.removeItem(STORAGE_KEY);
+  form.reset();
+}
+
+savedTextArea();
+
+// function savedTextArea() {
+//   const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+//   email.value = savedMessage.email || "";
+//   message.value = savedMessage.message || "";
+//   const newStorage = JSON.stringify(savedMessage);
+//   localStorage.setItem(STORAGE_KEY, newStorage);
+// };
+
+function savedTextArea() {
+  const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+  email.value = savedMessage.email || '';
+  message.value = savedMessage.message || '';
+};
