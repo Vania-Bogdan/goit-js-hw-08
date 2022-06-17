37d2b552de6dd58457a7a8ae8cd1a6d0a3837266
Vania Bogdan <bogdan.vania@gmail.com>
2022-06-17 13:01:22 +0300

    Create 01-gallery.js

---
+++ src/js/01-gallery.js
@@ -0,0 +1,21 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+// Change code below this line
+const gallery = document.querySelector(".gallery")
+
+const markup = galleryItems.map((galleryItem) =>
+    `<a class="gallery__item" href="${galleryItem.original}">
+        <img class="gallery__image"
+            src="${galleryItem.preview}"
+            alt="${galleryItem.description}"
+            />
+    </a>`)
+    .join("")
+
+gallery.insertAdjacentHTML("beforeend", markup)
+
+new SimpleLightbox('.gallery a', { captionsData: "alt", captionDelay: 250 })
